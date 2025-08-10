ca39899f8847180a7a3e4fecda7d2ab6b57170af
Pikruy <fikurysteam@gmail.com>
2025-08-10 13:56:22 +0700

    Update WindUITest.lua

--- WindUITest.lua
+++ WindUITest.lua
@@ -4709,9 +4709,9 @@ h.Icon, h.Icon .. ":" .. h.Title, 0, g.Window.Folder, h.__type, true)
                     ac("UIGradient", {
                         Rotation = 80,
                         Color = ColorSequence.new{
-                            ColorSequenceKeypoint.new(0.0, Color3.fromRGB(255, 255, 255)),
-                            ColorSequenceKeypoint.new(0.5, Color3.fromRGB(255, 255, 255)),
-                            ColorSequenceKeypoint.new(1.0, Color3.fromRGB(255, 255, 255)),
+                            ColorSequenceKeypoint.new(0, Color3.fromHex("#9D00FF")),
+                            ColorSequenceKeypoint.new(0.5, Color3.fromHex("#6B00CC")),
+                            ColorSequenceKeypoint.new(1, Color3.fromHex("#002FFF"))
                         },
                         Transparency = NumberSequence.new{
                             NumberSequenceKeypoint.new(0.0, 0.1),
