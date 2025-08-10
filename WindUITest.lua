749c750c9622a89a0465e9c8a686c2061a63c91d
Pikruy <fikurysteam@gmail.com>
2025-08-10 14:03:37 +0700

    Update WindUITest.lua

--- WindUITest.lua
+++ WindUITest.lua
@@ -4710,8 +4710,8 @@ h.Icon, h.Icon .. ":" .. h.Title, 0, g.Window.Folder, h.__type, true)
                         Rotation = 80,
                         Color = ColorSequence.new{
                             ColorSequenceKeypoint.new(0, Color3.fromHex("#9D00FF")),
-                            ColorSequenceKeypoint.new(0.5, Color3.fromHex("#6B00CC")),
-                            ColorSequenceKeypoint.new(1.0, Color3.fromRGB(255, 255, 255)),
+                            ColorSequenceKeypoint.new(3.0, Color3.fromHex("#6B00CC")),
+                            ColorSequenceKeypoint.new(5.0, Color3.fromRGB(255, 255, 255)),
                         },
                         Transparency = NumberSequence.new{
                             NumberSequenceKeypoint.new(0.0, 0.1),
