02164e73def87279f875f235224476fa009828d0
Pikruy <fikurysteam@gmail.com>
2025-08-10 13:06:33 +0700

    Update WindUITest.lua

--- WindUITest.lua
+++ WindUITest.lua
@@ -2864,10 +2864,10 @@ h.Image, h.Title, h.UICorner - 3, g.Window.Folder, "Image", h.Color and true or 
                         BorderSizePixel = 0,
                     }, {
                         e("UIGradient", {
-                            Rotation = 45,
+                            Rotation = 0,
                             Color = ColorSequence.new{
                                 ColorSequenceKeypoint.new(0, Color3.fromHex("#9D00FF")),
-                                ColorSequenceKeypoint.new(1, Color3.fromHex("#00E5FF"))
+                                ColorSequenceKeypoint.new(1, Color3.fromHex("#4400ff"))
                             }
                         })
                     }),
