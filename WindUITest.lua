0b87b90f0016ad29ac8d03f077bb5546b2218a77
Pikruy <fikurysteam@gmail.com>
2025-08-13 12:57:47 +0700

    Update WindUITest.lua

--- WindUITest.lua
+++ WindUITest.lua
@@ -6770,10 +6770,10 @@ do
             end
             function o.Divider(L)
                 local M = b("Frame", {
-                    Size = UDim2.new(1, 0, 0, 15),
+                    Size = UDim2.new(1, 0, 0, 1),
                     Position = UDim2.new(0.5, 0, 0, 0),
                     AnchorPoint = Vector2.new(0.5, 0),
-                    BackgroundTransparency = .5,
+                    BackgroundTransparency = .9,
                     ThemeTag = {
                         BackgroundColor3 = "Text"
                     }
