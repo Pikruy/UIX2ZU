9202d3b1af0fde13dc64aec0c7181e113d9a432b
Pikruy <fikurysteam@gmail.com>
2025-08-14 13:36:40 +0700

    Update WindUITest.lua

--- WindUITest.lua
+++ WindUITest.lua
@@ -2472,6 +2472,44 @@ do
                 Visible = false,
                 Active = false,
                 ZIndex = 9999999,
+            }, {
+                c('Frame', {
+                    Size = UDim2.new(1, h.UIPadding, 1, h.UIPadding),
+                    BackgroundTransparency = 1,
+                    Position = UDim2.new(0.5, 0, 0.5, 0),
+                    AnchorPoint = Vector2.new(0.5, 0.5),
+                    ZIndex = 999999,
+                    Name = 'Lock'
+                }, {
+                    c('UICorner', {
+                        CornerRadius = UDim.new(0, 11)
+                    }),
+                    c('ImageLabel', {
+                        Image = b.Icon'lock'[1],
+                        ImageRectOffset = b.Icon'lock'[2].ImageRectPosition,
+                        ImageRectSize = b.Icon'lock'[2].ImageRectSize,
+                        Size = UDim2.new(0, 22, 0, 22),
+                        ImageTransparency = 1,
+                        BackgroundTransparency = 1,
+                        Active = false
+                    }),
+                    c('TextLabel', {
+                        BackgroundTransparency = 1,
+                        Text = 'Locked',
+                        TextTransparency = 1,
+                        AutomaticSize = 'XY',
+                        FontFace = Font.new(b.Font, Enum.FontWeight.SemiBold),
+                        TextSize = 16,
+                        Active = false,
+                        TextColor3 = Color3.new(1, 1, 1)
+                    }),
+                    c('UIListLayout', {
+                        Padding = UDim.new(0, h.UIPadding),
+                        FillDirection = 'Horizontal',
+                        VerticalAlignment = 'Center',
+                        HorizontalAlignment = 'Center'
+                    })
+                })
             })
             h.UIElements.Main = e(h.UICorner, "Squircle", {
                 Size = UDim2.new(1, 0, 0, 50),
@@ -2526,11 +2564,19 @@ do
                 k = false
                 h.UIElements.Locked.Active = true
                 h.UIElements.Locked.Visible = true
+
+                -- Biar icon & teks nongol
+                h.UIElements.Locked.Lock.ImageLabel.ImageTransparency = 0
+                h.UIElements.Locked.Lock.TextLabel.TextTransparency = 0
             end
             function h.Unlock(s)
                 k = true
                 h.UIElements.Locked.Active = false
                 h.UIElements.Locked.Visible = false
+
+                -- Balikin jadi transparan lagi kalau mau
+                h.UIElements.Locked.Lock.ImageLabel.ImageTransparency = 1
+                h.UIElements.Locked.Lock.TextLabel.TextTransparency = 1
             end
             return h
         end
