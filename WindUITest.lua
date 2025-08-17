a21fcd2a6e7fb389dd1b36a45ed092d5e0fb452d
Pikruy <fikurysteam@gmail.com>
2025-08-17 21:54:12 +0700

    Update WindUITest.lua

--- WindUITest.lua
+++ WindUITest.lua
@@ -3812,7 +3812,7 @@ do
                     end)
                     RecalculateCanvasSize()
                     RecalculateListSize()
-                    if w % 10 == 0 then
+                    if w % 5 == 0 then
                         task.wait()
                     end
                 end
