73de749c0b644ac1ba7baddcd5b3d9929706ebda
Pikruy <fikurysteam@gmail.com>
2025-08-18 09:27:18 +0700

    Update WindUITest.lua

--- WindUITest.lua
+++ WindUITest.lua
@@ -3812,9 +3812,8 @@ do
                     end)
                     RecalculateCanvasSize()
                     RecalculateListSize()
-                    local step = game:GetService("UserInputService").TouchEnabled and 4 or 7
-                    if w % step == 0 then
-                        task.wait()
+                    if w % 20 == 0 then
+                        game:GetService("RunService").Heartbeat:Wait()
                     end
                 end
                 local y = 0
